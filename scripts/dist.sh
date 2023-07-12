67ed38ac3783119adcfae75ca3778f115f9e14a5
lothrazar <samson.bassett@gmail.com>
2023-07-12 08:07:09 -0700

    0.0.7 release branch kickoff

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -2,30 +2,34 @@
 
 #./gradlew copyJar 
 # TODO loop on list lol
-cp -R build/libs/. c:/temp
-cp -R build/libs/. ../ElementaryOres/libs
-cp -R build/libs/. ../PotatoBread/libs
-cp -R build/libs/. ../TinyLightBulbs/libs
-cp -R build/libs/. ../CustomGameRules/libs
+# cp -R build/libs/. c:/temp
 
-cp -R build/libs/. ../ForgeCreeperHeal/libs
-cp -R build/libs/. ../EnchantingRunes/libs
-cp -R build/libs/. ../ForgeTemplate/libs
-cp -R build/libs/. ../Scraps/libs
-cp -R build/libs/. ../SaplingGrowthControl/libs
-cp -R build/libs/. ../cobblestoney/libs
-cp -R build/libs/. ../absentbydesign/libs
-cp -R build/libs/. ../antibonemeal/libs
-cp -R build/libs/. ../BlockLayers/libs
-cp -R build/libs/. ../AutoRun/libs
-cp -R build/libs/. ../FragileTorches/libs
-cp -R build/libs/. ../GardenTools/libs
 cp -R build/libs/. ../Cyclic/libs
-cp -R build/libs/. ../GlassCutter/libs
-cp -R build/libs/. ../FragileTorches/libs
+cp -R build/libs/. ../RootsClassic/libs
+cp -R build/libs/. ../Similsax-Transtructors/libs
+cp -R build/libs/. ../SimpleTomb/libs
+cp -R build/libs/. ../Storage-Network/libs
 
-cp -R build/libs/. ../HeartBalance/libs
-
-cp -R build/libs/. ../NoLogPunch/libs
-cp -R build/libs/. ../NoLavaBuild/libs
-cp -R build/libs/. ../stupidHorseStandStill/libs
+# cp -R build/libs/. ../Cyclic/libs
+# cp -R build/libs/. ../ElementaryOres/libs
+# cp -R build/libs/. ../PotatoBread/libs
+# cp -R build/libs/. ../TinyLightBulbs/libs
+# cp -R build/libs/. ../CustomGameRules/libs
+# cp -R build/libs/. ../ForgeCreeperHeal/libs
+# cp -R build/libs/. ../EnchantingRunes/libs
+# cp -R build/libs/. ../ForgeTemplate/libs
+# cp -R build/libs/. ../Scraps/libs
+# cp -R build/libs/. ../SaplingGrowthControl/libs
+# cp -R build/libs/. ../cobblestoney/libs
+# cp -R build/libs/. ../absentbydesign/libs
+# cp -R build/libs/. ../antibonemeal/libs
+# cp -R build/libs/. ../BlockLayers/libs
+# cp -R build/libs/. ../AutoRun/libs
+# cp -R build/libs/. ../FragileTorches/libs
+# cp -R build/libs/. ../GardenTools/libs
+# cp -R build/libs/. ../GlassCutter/libs
+# cp -R build/libs/. ../FragileTorches/libs
+# cp -R build/libs/. ../HeartBalance/libs
+# cp -R build/libs/. ../NoLogPunch/libs
+# cp -R build/libs/. ../NoLavaBuild/libs
+# cp -R build/libs/. ../stupidHorseStandStill/libs
