780a81219892e32c2479cd6a1d427a0cb480d197
lothrazar <samson.bassett@gmail.com>
2023-07-15 22:18:10 -0700

    0.0.7 fluid & energy gui widgets

---
+++ src/main/java/com/lothrazar/library/packet/PacketItemToggle.java
@@ -0,0 +1,52 @@
+package com.lothrazar.library.packet;
+
+import java.util.function.Supplier;
+import com.lothrazar.library.core.IHasClickToggle;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.network.NetworkEvent;
+
+public class PacketItemToggle extends PacketFlib {
+
+  private int slot;
+
+  public PacketItemToggle(int slot) {
+    this.slot = slot;
+  }
+
+  public static void handle(PacketItemToggle message, Supplier<NetworkEvent.Context> ctx) {
+    ctx.get().enqueueWork(() -> {
+      ServerPlayer player = ctx.get().getSender();
+      if (player.containerMenu == null) {
+        return;
+      }
+      int scount = player.containerMenu.slots.size();
+      //this is an edge case but it DID happen: put charmin your hotbar and then open a creative inventory tab. avoid index OOB
+      if (message.slot >= scount) {
+        //will NOT work in creative mode. slots are messed up
+        return;
+      }
+      Slot slotObj = player.containerMenu.getSlot(message.slot);
+      if (slotObj != null && !slotObj.getItem().isEmpty()) {
+        ItemStack maybeCharm = slotObj.getItem();
+        if (maybeCharm.getItem() instanceof IHasClickToggle) {
+          //example: is a charm or something
+          IHasClickToggle c = (IHasClickToggle) maybeCharm.getItem();
+          c.toggle(player, maybeCharm);
+        }
+      }
+    });
+    message.done(ctx);
+  }
+
+  public static PacketItemToggle decode(FriendlyByteBuf buf) {
+    PacketItemToggle p = new PacketItemToggle(buf.readInt());
+    return p;
+  }
+
+  public static void encode(PacketItemToggle msg, FriendlyByteBuf buf) {
+    buf.writeInt(msg.slot);
+  }
+}
