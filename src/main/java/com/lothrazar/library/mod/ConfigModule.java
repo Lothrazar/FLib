92b81120063ea881a26912dfce8eef59dc174f46
Sam <lothrazar@hotmail.com>
2026-04-10 22:32:17 -0700

    rm outdated bashscripts

--- src/main/java/com/lothrazar/library/mod/ConfigModule.java
+++ src/main/java/com/lothrazar/library/mod/ConfigModule.java
@@ -1,20 +1,19 @@
 package com.lothrazar.library.mod;
 
 import com.lothrazar.library.FutureLibMod;
-import com.lothrazar.library.config.ConfigTemplate;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
 import net.neoforged.neoforge.common.ModConfigSpec.Builder;
 
-public class ConfigModule extends ConfigTemplate {
+public class ConfigModule {
 
   private static final ModConfigSpec CONFIG;
   public static BooleanValue ENABLE_COMMANDS;
 
   static {
-    final Builder builder = builder();
+    final Builder builder = new ModConfigSpec.Builder();
     builder.comment("General settings").push(FutureLibMod.MODID);
     ENABLE_COMMANDS = builder.comment("If true, the /flib command will be registered").define("command.enabled", true);
     builder.pop();
